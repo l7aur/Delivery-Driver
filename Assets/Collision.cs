6cf5d042f0bf47e04b31cdd4f210c6d98ce73a31
l7aur <gradlaur@yahoo.com>
2024-08-01 16:51:35 +0300

    woork1

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
+    private bool hasPackage = false;
+    [SerializeField] float customerDestroyDelay = 0.3f;
+    [SerializeField] float packageDestroyDelay = 0.1f;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         UnityEngine.Debug.Log("hit");
@@ -12,10 +16,17 @@ public class Collision : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Package")) 
+        if (collision.CompareTag("Package") && !hasPackage)
+        {
+            hasPackage = true;
             UnityEngine.Debug.Log("Package picked up!");
-
-        if (collision.CompareTag("Customer"))
+            Destroy(collision.gameObject, packageDestroyDelay);
+        }
+        if (collision.CompareTag("Customer") && hasPackage)
+        {
+            hasPackage = false;
             UnityEngine.Debug.Log("Package delivered!");
+            Destroy(collision.gameObject, customerDestroyDelay);
+        }
     }
 }
